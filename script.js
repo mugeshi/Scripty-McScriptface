437dde55b0c2ff17968d6878fc079642346dff0b
mugeshi <nicolemugeshi@gmail.com>
2024-04-18 17:46:52 +0300

    updated prioritySelect

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
   const taskList = document.getElementById("taskList");
 
   addTaskBtn.addEventListener("click", function() {
+    console.log("Add Task button clicked");
     const taskText = taskInput.value.trim();
     const priorityValue = prioritySelect.value;
     if (taskText !== "") {
+        console.log("Task list item clicked");
       const li = document.createElement("li");
       li.textContent = taskText;
       li.classList.add(priorityValue);
       taskList.appendChild(li);
       taskInput.value = "";
+      console.log("Task added:", taskText, "Priority:", priorityValue);
     } else {
       alert("Please enter a task!");
     }
+    
   });
 
   taskList.addEventListener("click", function(e) {
+   
     if (e.target.tagName === "LI") {
       e.target.remove();
     }
