fb30f6f235cedef8fa50ba713cb47c5a86a5f58a
mugeshi <nicolemugeshi@gmail.com>
2024-04-25 22:16:30 +0300

    updated hoisting

---
+++ Hoisting.js
@@ -0,0 +1,8 @@
+const sumConst = (a, b) => a + b;
+
+console.log(`sumFunc(1, 2) => ${sumFunc(1, 2)}`);
+console.log(`sumConst(3, 4) => ${sumConst(3, 4)}`);
+
+function sumFunc(a, b) {
+    return a + b;
+}
