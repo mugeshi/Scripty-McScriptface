85c8d9acd27cdb4fe16078040ca0715fd8e76b94
mugeshi <nicolemugeshi@gmail.com>
2024-04-26 23:41:09 +0300

    updated timeout

---
+++ loop.js
@@ -0,0 +1,7 @@
+// Define an array of numbers
+const numbers = [1, 2, 3, 4, 5];
+
+// Iterate over each number in the array using a for loop
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
